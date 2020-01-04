3e4e8734fff606c213e973fa3e1e77e2b9c36779
Wellyson Martins <wellysonmartins@gmail.com>
2020-01-04 14:59:23 -0300

    React + Webpack

--- Modulo02/react-webpack/src/app.js
+++ Modulo02/react-webpack/src/app.js
@@ -1,11 +1,11 @@
 'use strict'
 
-var React = require('react')
+import React from 'react'
 
-var Title = React.createClass({
-   render: function() {
-      return React.createElement('h1', null, 'Título')
-   }
+const App = React.createClass({
+  render: function () {
+    return <h1>Aplicação</h1>
+  }
 })
 
-module.exports = Title
+export default App
--- Modulo02/react-webpack/src/index.js
+++ Modulo02/react-webpack/src/index.js
@@ -1,10 +1,24 @@
 'use strict'
 
-var React = require('react')
-var ReactDOM = require('react-dom')
-var Title = require('./app')
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './app'
 
-ReactDOM.render(
-   React.createElement(Title),
-   document.querySelector('[data-js="app"]')
-)
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <NextApp />
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
+}
