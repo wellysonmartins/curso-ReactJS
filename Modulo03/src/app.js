6931440d29f5642e4642c4b5c94a41d158729979
Wellyson Martins <wellysonmartins@gmail.com>
2020-01-06 16:44:41 -0300

    Events

--- Modulo03/src/app.js
+++ Modulo03/src/app.js
@@ -7,10 +7,12 @@ import Square from './square'
 class App extends React.Component {
   render () {
     return (
-      <div className='container'>
-        {['blue', 'red', 'green'].map((square) => (
-          <Square key={square} color={square} />
-        ))}
+      <div
+        className='container' onClick={function (e) {
+          alert('clicou')
+        }}
+      >
+        <Square />
       </div>
     )
   }
