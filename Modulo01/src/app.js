4d1f0a520ff5bb01ad07f1e1457b8e760ce9d4e2
Wellyson Martins <wellysonmartins@gmail.com>
2020-02-12 13:24:23 -0300

    State - Component

--- Modulo01/src/app.js
+++ Modulo01/src/app.js
@@ -1,13 +1,31 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './title'
+import Timer from './timer'
 
 class App extends Component {
+  constructor () {
+    console.log('constructor')
+    super()
+    this.state = {
+      showTimer: true
+    }
+  }
+
+  componentWillMount () {
+    console.log('componentWillMount')
+  }
+
+  componentDidMount () {
+    console.log('componentDidMount')
+  }
+
   render () {
+    console.log('render')
     return (
-      <div className='container'>
-        <Title name='Wellyson' />
+      <div>
+        {this.state.showTimer && <Timer />}
+        <button onClick={() => { this.setState({ showTimer: !this.state.showTimer }) }}>Show / hide timer</button>
       </div>
     )
   }
